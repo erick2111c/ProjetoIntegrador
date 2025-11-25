e77d47713f2cdea157f73f4c4dab73d352705734
VICAEL VALENTIM KUHN <04287731093@senacrs.edu.br>
2025-11-25 20:43:17 -0300

    Finalizar

--- ComprarTenis.xaml.cs
+++ ComprarTenis.xaml.cs
@@ -29,5 +29,10 @@ namespace WpfApp1
         {
 
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new FinalizarCompra());
+        }
     }
 }
